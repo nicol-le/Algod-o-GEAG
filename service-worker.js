918c1a28688f368c642cd139690f68a09b9f3142
nicol-le <gomesnocolle@gmail.com>
2025-04-04 19:53:09 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,39 @@
 const CACHE_NAME = 'entomoffauna-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/favicon.ico',
-    '/manifest.json',
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/favicon.ico',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
-
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-  
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-    );
+  self.skipWaiting(); // Instala imediatamente
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', (event) => {
- 
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  self.clients.claim(); // Assume controle imediato
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
